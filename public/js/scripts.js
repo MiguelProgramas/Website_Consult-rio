70f205c1bedebf35712a0114698e92439f70c236
MiguelProgramas <miguel.p.rograma.lima.tsis@gmail.com>
2024-05-27 00:44:54 -0300

    Lógica de conclusão de diagnóstico com base nas respostas do usuário concluída. Algumas perguntas foram removidas para evitar conclusões travadas em apenas dois cenários possíveis (por exemplo, antes da resolução do problema, era sempre ou HPV, ou Sífilis).

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,45 @@
-function respGenero() {
+function respGenero1() {
+  document.getElementById("pergunta3").style.display = "block";
+}
+
+function respGenero2() {
   document.getElementById("pergunta2").style.display = "block";
 }
 
-function respFerida() {
+function respAbd() {
+  document.getElementById("pergunta3").style.display = "block";
+}
+
+function respUrin() {
+  document.getElementById("pergunta4").style.display = "block";
+}
+
+function respDorUrin() {
+  document.getElementById("pergunta5").style.display = "block";
+}
+
+function respCab() {
   document.getElementById("pergunta6").style.display = "block";
+}
+
+function respArd() {
+  document.getElementById("pergunta7").style.display = "block";
+}
+
+function respSan() {
+  document.getElementById("pergunta8").style.display = "block";
+}
+
+function respFerida1() {
+  document.getElementById("pergunta9").style.display = "block";
+}
+
+function respFerida2() {
+  document.getElementById("finalizacao").style.display = "block";
+}
+
+function respTipoFeri() {
+  document.getElementById("finalizacao").style.display = "block";
 }
 
 function conclusao() {
@@ -25,34 +61,36 @@ function conclusao() {
   return true;
 }
 
-function respAbd1() {
-  document.getElementById("pergunta7").style.display = "block";
+function final() {
+  let resposta2 = document.getElementsByName("dorAbdominal");
+  let resposta3 = document.getElementsByName("vontadeUrin");
+  let resposta4 = document.getElementsByName("dorUrin");
+  let resposta5 = document.getElementsByName("dorCabeca");
+  let resposta7 = document.getElementsByName("sangUrin");
+  let resposta9 = document.getElementsByName("tipoFeri");
+
+  if (resposta9[2].checked == true || resposta9[0].checked == true) {
+    alert("O seu hipotético diagnóstico é: Danovanose");
+  }
+
+  else if (resposta7[0].checked == true) {
+    alert("O seu hipotético diagnóstico é: Cancro Mole");
+  }
+
+  else if (resposta5[1].checked == true) {
+    alert("O seu hipotético diagnóstico é: HPV");
+  }
+
+  else if (resposta4[0].checked == true) {
+    alert("O seu hipotético diagnóstico é: Tricomeníase");
+  }
+
+  else if (resposta3[0].checked == true) {
+    alert("O seu hipotético diagnóstico é: Gonorréia");
+  }
+
+  else if (resposta2[0].checked == true) {
+    alert("O seu hipotético diagnóstico é: Clamídia");
+  }
+
 }
-
-function respUrin(){
-  document.getElementById("pergunta8").style.display = "block";
-}
-
-function respDor(){
-  document.getElementById("pergunta9").style.display = "block";
-}
-
-function respVer(){
-  document.getElementById("pergunta4").style.display = "block";
-}
-
-function respCab(){
-    document.getElementById("pergunta10").style.display = "block";
-}
-
-function respArd(){
-  document.getElementById("pergunta11").style.display = "block";
-}
-
-
-
-
-function apagarMensagem() {
-  document.getElementById("mensagem").innerHTML = "";
-}
-
